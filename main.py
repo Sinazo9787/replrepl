0216a4bce312331df25b68284a9f2866635a9e82
Sinazo  Vutuza <83216975+Sinazo9787@users.noreply.github.com>
2022-06-09 13:14:12 +0000

    enumerate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#counter
-my_list = [1,2,3,4,5,6,7,8,9,10]
-
-counter = 0
-for item in my_list:
-  counter = counter + item
-
-print(counter)
+#enumerate
+for i,char in enumerate(list(range(100))):
+  print(i,char)
+  if char == 50:
+    print(f'index of 50 is: {i}')
+                         
